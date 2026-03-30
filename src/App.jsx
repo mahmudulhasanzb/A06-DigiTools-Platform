fbc8ebaf04356d0e81b0b839920316468bd24e00
mahmudulhasanzb <mahmudulhasankk9@gmail.com>
2026-03-30 11:18:31 +0600

    Project initiated with all the resoursec

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { FaBeer } from 'react-icons/fa'
+import './App.css'
+import NavBar from './components/NavBar/NavBar'
+import Banner from './components/Banner/Banner'
+import Footer from './components/Footer/Footer'
+
+function App() {
+  return (
+    <>
+      <NavBar></NavBar>
+      <Banner></Banner>
+
+      {/* footer */}
+      <Footer></Footer>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Banner/Banner.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import bannerImg from '../../assets/banner.png';
+
+const Banner = () => {
+  return (
+    <>
+
+    </>
+  );
+};
+
+export default Banner;
