be3337a6e8aced071e3d8ae6e08c4e286bf483e0
mahmudulhasanzb <mahmudulhasankk9@gmail.com>
2026-03-30 11:20:23 +0600

    Navbar Design Added

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -3,6 +3,51 @@ import React from 'react';
 const NavBar = () => {
   return (
     <>
+      <div className="navbar bg-base-100 shadow-sm max-w-7xl mx-auto">
+        <div className="navbar-start">
+          <h1 className="text-4xl font-semibold">DigiTools</h1>
+        </div>
+        <div className="navbar-center">
+          <ul className="flex gap-8">
+            <li>
+              <a href="#">Products</a>
+            </li>
+            <li>
+              <a href="#">Features</a>
+            </li>
+            <li>
+              <a href="#">Pricing</a>
+            </li>
+            <li>
+              <a href="#">Testimonials</a>
+            </li>
+            <li>
+              <a href="#">FAQ</a>
+            </li>
+          </ul>
+        </div>
+        <div className="navbar-end">
+          <button className="btn btn-ghost btn-circle">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-5 w-5"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              {' '}
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z"
+              />{' '}
+            </svg>
+          </button>
+          <button className="btn">Login</button>
+          <button className="btn btn-primary rounded-full bg">Get Started</button>
+        </div>
+      </div>
     </>
   );
 };
